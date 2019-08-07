8467660488545a9fa7bd6bc4be2d8c46dba9c943
Hévlla Oliveira Souza <32911113+hevlla@users.noreply.github.com>
2019-08-07 19:20:03 -0300

    Update 100_The3n_1.py
    O código está aceitando múltipla entradas, porém lento, ultrapassando os 3 segundos máximos exigido na plataforma (UVA).

--- Contest 1/100_The3n_1.py
+++ Contest 1/100_The3n_1.py
@@ -1,34 +1,25 @@
 def comparacao(operacao, lista):
-  contador = 1
-  while operacao != 1:
-    if (operacao % 2) == 0:
-        operacao = operacao / 2
-        contador += 1
-    else:
-        operacao = 3 * operacao + 1
-        contador += 1
-  lista.append(contador)
+    contador = 1
+    while operacao != 1:
+        if (operacao % 2) == 0:
+            operacao = operacao / 2
+            contador += 1
+        else:
+            operacao = 3 * operacao + 1
+            contador += 1
+        lista.append(contador)
 
 def operacao(inicial, final):
     maior = []
-
     while True:
-      if inicial == final:
-        comparacao(inicial, maior)
+        for i in range(min(inicial, final), max(inicial, final)+1):
+            comparacao(i, maior)
+            i += 1
         return max(maior)
+
+while True:
+    try:
+        inicio, fim = map(int, input().split())
+        print(inicio, fim, operacao(inicio, fim))
+    except EOFError:
         break
-
-      else:
-        if final < inicial:
-          while inicial != final:
-            comparacao(final, maior)
-            final += 1
-        else:
-          while inicial != final:
-              comparacao(inicial, maior)
-              inicial += 1
-
-entrada = input().split()
-inicio = int(entrada[0])
-fim = int(entrada[1])
-print(inicio, fim, operacao(inicio, fim))
