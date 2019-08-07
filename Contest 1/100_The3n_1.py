0ecb31ba6205e9c5c79eb2e467f78b93790fb481
Hévlla Oliveira Souza <32911113+hevlla@users.noreply.github.com>
2019-08-07 20:03:53 -0300

    Update 100_The3n_1.py
    O tempo excessivo se dava por conta que valores já calculados não eram armazenados. Assim o programa tinha que calcular novamente, causando desperdício de tempo. 
    Então coloquei para que todos os números fossem calculados até o meu final e assim armazenados em um dicionário, e caso aparece o número novamente, seria apenas consultado no dicionário.

--- Contest 1/100_The3n_1.py
+++ Contest 1/100_The3n_1.py
@@ -1,25 +1,27 @@
-def comparacao(operacao, lista):
-    contador = 1
-    while operacao != 1:
-        if (operacao % 2) == 0:
-            operacao = operacao / 2
-            contador += 1
-        else:
-            operacao = 3 * operacao + 1
-            contador += 1
-        lista.append(contador)
+def comparacao(operacao):
+    if operacao in listados:
+        return listados[operacao]
+    if operacao <= 1:
+        return 1
+    if (operacao % 2) == 0:
+        valor = operacao / 2
+    else:
+        valor = 3 * operacao + 1
+    listados[operacao] = comparacao(valor) + 1
+    return listados[operacao]
 
-def operacao(inicial, final):
-    maior = []
-    while True:
-        for i in range(min(inicial, final), max(inicial, final)+1):
-            comparacao(i, maior)
-            i += 1
-        return max(maior)
+def operacao():
+    maior = 0
+    for i in range(min(inicio,fim), max(inicio, fim) + 1):
+        resul = comparacao(i)
+        if resul > maior:
+            maior = resul
+    return maior
 
+listados = {}
 while True:
     try:
         inicio, fim = map(int, input().split())
-        print(inicio, fim, operacao(inicio, fim))
+        print(inicio, fim, operacao())
     except EOFError:
         break
