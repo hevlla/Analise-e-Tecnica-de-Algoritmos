ba55f68b6b978c27143bbc5e556b8a3c364cf19d
Hévlla Oliveira Souza <32911113+hevlla@users.noreply.github.com>
2019-08-25 21:29:34 -0300

    Update 1110_Jogando_Cartas_Fora.py
    Atualização para imprimir da forma esperada.

--- Contest_1 - Substituicao/1110_Jogando_Cartas_Fora.py
+++ Contest_1 - Substituicao/1110_Jogando_Cartas_Fora.py
@@ -1,27 +1,29 @@
 baralho = list()
+cartas_descartadas = list()
 
 def cria_cartas(qtd_cartas):
-    for carta in range(qtd_cartas):
-        carta += 1
+    baralho.clear()
+    for carta in range(1, qtd_cartas+1):
         baralho.append(carta)
     return baralho
 
 def distribuindo_cartas():
-    cartas_descartadas = list()
-    if len(baralho) == 1:
-        return cartas_descartadas
+    cartas_descartadas.clear()
     while len(baralho) != 1:
-        cartas_descartadas.append(baralho[0])
+        cartas_descartadas.append(str(baralho[0]))
         baralho.pop(0)
-        baralho.append(baralho[0])
+        baralho.append(str(baralho[0]))
         baralho.pop(0)
-    return cartas_descartadas
 
+def main():
+    while True:
+        qtd_cartas = int(input())
+        if qtd_cartas == 0 or qtd_cartas > 50:
+            break
+        cria_cartas(qtd_cartas)
+        distribuindo_cartas()
+        print("Discarded cards: " +", ".join(cartas_descartadas), end="")
+        print("\nRemaining card:", str(baralho[0]), end="\n")
 
-
-while True:
-    qtd_cartas = int(input())
-    if qtd_cartas == 0 or qtd_cartas > 50:
-        break
-    cria_cartas(qtd_cartas)
-    print("Discarded cards: ", distribuindo_cartas(), "\nRemaining card: ", baralho)
+if __name__ == "__main__":
+    main()
